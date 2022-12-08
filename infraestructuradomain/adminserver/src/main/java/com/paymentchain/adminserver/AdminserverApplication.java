8c7009fa37d1c85a293a6a851ee6838309a90a32
Gabriel Gomez <gabriel70@gmail.com>
2022-12-07 21:05:39 -0300

    adminserver DIP

--- infraestructuradomain/adminserver/src/main/java/com/paymentchain/adminserver/AdminserverApplication.java
+++ infraestructuradomain/adminserver/src/main/java/com/paymentchain/adminserver/AdminserverApplication.java
@@ -1,13 +1,25 @@
 package com.paymentchain.adminserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminserver
+@EnableEurekaClient
 public class AdminserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminserverApplication.class, args);
 	}
 
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityCofigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exceptio {
+			http.autorizeRequest().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
+	}
 }
