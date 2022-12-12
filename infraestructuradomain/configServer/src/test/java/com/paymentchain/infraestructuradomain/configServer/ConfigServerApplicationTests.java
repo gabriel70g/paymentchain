8841bec62f560363bab3e9dae8623b508be2d9c6
gabrielalejandrogomez <gabriel70@gmail.com>
2022-12-12 09:57:57 -0300

    Config proyect

---
+++ infraestructuradomain/configServer/src/test/java/com/paymentchain/infraestructuradomain/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentchain.infraestructuradomain.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
